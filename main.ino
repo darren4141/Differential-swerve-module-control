cc9e7a2b7a668ece89c6ce6bf942345f151b6a54
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-08-24 15:56:57 -0400

    a

--- main.ino
+++ main.ino
@@ -10,12 +10,11 @@ const int enablePin2 = 5;
 const int encA2 = 22;
 const int encB2 = 23;
 
-int encoderCount1 = 0;
-int encoderCount2 = 0;
+int encoderCount[] = {0, 0};
 
-float eI = 0;
-float ePPrevious = 0;
-float previousTime = 0;
+float eI[] = {0, 0};
+float ePPrevious[] = {0, 0};
+float previousTime[] = {0, 0};
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,69 +40,81 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(fwdPin1, HIGH);
-  digitalWrite(bwdPin1, LOW);
-
-  digitalWrite(fwdPin2, LOW);
-  digitalWrite(bwdPin2, HIGH);
-
-  analogWrite(enablePin1, 255);
-  analogWrite(enablePin2, 255);
-
-
-//  float pwr = calculatePID(1000, 1, 0.4, 0.01);
+//  digitalWrite(fwdPin1, HIGH);
+//  digitalWrite(bwdPin1, LOW);
 //
-//  if(pwr > 0){
-//    analogWrite(enablePin1, abs(pwr));
-//    digitalWrite(fwdPin1, HIGH);
-//    digitalWrite(bwdPin1, LOW);
-//  }else{
-//    analogWrite(enablePin1, abs(pwr));
-//    digitalWrite(fwdPin1, LOW);
-//    digitalWrite(bwdPin1, HIGH);
-//  }
+//  digitalWrite(fwdPin2, LOW);
+//  digitalWrite(bwdPin2, HIGH);
+//
+//  analogWrite(enablePin1, 255);
+//  analogWrite(enablePin2, 255);
+
+
+  float pwr1 = calculatePID(0, 1000, 1, 0.4, 0.01);
+
+  if(pwr1 > 0){
+    analogWrite(enablePin1, abs(pwr1));
+    digitalWrite(fwdPin1, HIGH);
+    digitalWrite(bwdPin1, LOW);
+  }else{
+    analogWrite(enablePin1, abs(pwr1));
+    digitalWrite(fwdPin1, LOW);
+    digitalWrite(bwdPin1, HIGH);
+  }
+
+  float pwr2 = calculatePID(1, 1000, 1, 0.4, 0.01);
+
+  if(pwr2 > 0){
+    analogWrite(enablePin2, abs(pwr2));
+    digitalWrite(fwdPin2, HIGH);
+    digitalWrite(bwdPin2, LOW);
+  }else{
+    analogWrite(enablePin2, abs(pwr2));
+    digitalWrite(fwdPin2, LOW);
+    digitalWrite(bwdPin2, HIGH);
+  }
   
-  Serial.print(encoderCount1);
-//  Serial.print(", ");
-//  Serial.println(pwr);
+  Serial.print(encoderCount[0]);
+  Serial.print(", ");
+  Serial.println(encoderCount[1]);
 }
 
 void tickEncoder1(){
   if(digitalRead(encA1) > digitalRead(encB1)){
-    encoderCount1++;
+    encoderCount[0]++;
   }else{
-    encoderCount1--;
+    encoderCount[0]--;
   }
 }
 
 void tickEncoder2(){
   if(digitalRead(encA2) > digitalRead(encB2)){
-    encoderCount2++;
+    encoderCount[1]++;
   }else{
-    encoderCount2--;
+    encoderCount[1]--;
   }
 }
 
-//float calculatePID(int target, float kP, float kI, float kD){
-//  long currentTime = micros();
-//  float dT = ((float)(currentTime - previousTime)) / 1.0e6;
-//
-//  int eP = target - encoderCount;
-//  eI = eI + (eP * dT);
-//  float eD = (eP - ePPrevious)/dT;
-//
-//  previousTime = currentTime;
-//  ePPrevious = eP;
-//
-//  float pwr = (kP * eP) + (kI * eI) + (kD * eD);
-//
-//  if(abs(pwr) > 255){
-//    if(pwr > 0){
-//      pwr = 255;
-//    }else{
-//      pwr = -255;
-//    }
-//  }
-//
-//  return pwr;
-//}
+float calculatePID(int motorNum, int target, float kP, float kI, float kD){
+  long currentTime = micros();
+  float dT = ((float)(currentTime - previousTime[motorNum])) / 1.0e6;
+
+  int eP = target - encoderCount[motorNum];
+  eI[motorNum] = eI[motorNum] + (eP * dT);
+  float eD = (eP - ePPrevious[motorNum])/dT;
+
+  previousTime[motorNum] = currentTime;
+  ePPrevious[motorNum] = eP;
+
+  float pwr = (kP * eP) + (kI * eI[motorNum]) + (kD * eD);
+
+  if(abs(pwr) > 255){
+    if(pwr > 0){
+      pwr = 255;
+    }else{
+      pwr = -255;
+    }
+  }
+
+  return pwr;
+}
