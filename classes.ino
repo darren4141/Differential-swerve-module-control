0439e866798e33125e1c5658cd60a367028ae6d7
Darren Liu <darrrenliu@gmail.com>
2024-12-29 03:42:02 -0500

    encoder class not really working

--- classes.ino
+++ classes.ino
@@ -12,10 +12,54 @@ const int encB2 = 23;
 
 int encoderCount[] = {0, 0};
 
+// class Encoder{
+  
+//   public:
+//     int encPinA;
+//     int encPinB;
+//     int count;
+//     Encoder(int new_encPinA, int new_encPinB): encPinA(new_encPinA), encPinB(new_encPinB), count(0){
+//       pinMode(encPinA, INPUT);
+//       pinMode(encPinB, INPUT);
+//     }
+    
+//     int getCount(){
+//       // Serial.println(count);
+//       return count;
+//     }
+//     void incEncoder(){
+//       count++;
+//     }
+
+//     // static void handleInterrupt(){
+//     //   Serial.println("a");
+//     //   if(instance){
+//     //     instance->tickEncoder();
+//     //   }
+//     // }
+
+//     // void tickEncoder(){
+//     //   Serial.print("ticking encoder ");
+//     //   Serial.println(encPinA);
+//     //   if(digitalRead(encPinA) > digitalRead(encPinB)){
+//     //     count++;
+//     //   }else{
+//     //     count--;
+//     //   }
+//     // }
+
+//     // static void setInstance(Encoder *new_instance){
+//     //   instance = new_instance;
+//     // }
+
+
+
+//   private:
+// };
+
 class Motor{
   public:
-    Motor(int new_fwdPin, int new_bwdPin, int new_enablePin): fwdPin(new_fwdPin), bwdPin(new_bwdPin), enablePin(new_enablePin)
-    // encoder = new_encoder
+    Motor(int new_fwdPin, int new_bwdPin, int new_enablePin, Encoder new_encoder): fwdPin(new_fwdPin), bwdPin(new_bwdPin), enablePin(new_enablePin), encoder(new_encoder)
     {
 
     }
@@ -41,36 +85,52 @@ class Motor{
     int fwdPin;
     int bwdPin;
     int enablePin;
-    // Encoder encoder;
+    Encoder encoder;
 };
 
-Motor motor1(2, 4, 15);
-Motor motor2(18, 19, 5);
+void tickEncoder1(){
+  Serial.print("ticking encoder ");
+  Serial.println(encA1);
+  if(digitalRead(encA1) > digitalRead(encB1)){
+    encoderCount[0]++;
+  }else{
+    encoderCount[0]--;
+  }
+}
+
+void tickEncoder2(){
+  Serial.print("ticking encoder ");
+  Serial.println(encA2);
+  if(digitalRead(encA2) > digitalRead(encB2)){
+    encoderCount[1]++;
+  }else{
+    encoderCount[1]--;
+  }
+}
+
+Encoder encoder1(16, 17);
+Encoder encoder2(22, 23);
+
+Motor motor1(2, 4, 15, encoder1);
+Motor motor2(18, 19, 5, encoder2);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   motor1.initialize();
   motor2.initialize();
+
+  // encoder1.setInstance(&encoder1);
+  // encoder2.setInstance(&encoder2);
+  attachInterrupt(digitalPinToInterrupt(16), tickEncoder1, RISING);
+  attachInterrupt(digitalPinToInterrupt(22), tickEncoder2, RISING);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   motor1.setPower(-150);
   motor2.setPower(150);
-
+  Serial.println(encoderCount[0]);
+  Serial.println(encoderCount[1]);
 }
-
-// class Encoder{
-//   int encPinA;
-//   int encPinB;
-//   int count;
-//   public:
-//     Encoder(int new_encPinA, int new_encPinB);
-// };
-
-// Encoder::Encoder():
-//   encPinA = new_encPinA,
-//   encPinB = new_encPinB{
-//     count = 0;
-
-// }
